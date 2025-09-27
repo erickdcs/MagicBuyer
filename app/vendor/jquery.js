c82b6c2e2e26044a142c3e8c257ee0bf5f292e91
erickdcs <51513004+erickdcs@users.noreply.github.com>
2025-09-27 21:23:16 +0200

    Prepare dist assets for extension packaging

--- app/vendor/jquery.js
+++ app/vendor/jquery.js
@@ -1,7 +1,20 @@
-import jquery from "jquery";
-
 const globalScope = typeof window !== "undefined" ? window : globalThis;
+
+const jquery = (() => {
+  if (globalScope.jQuery) {
+    return globalScope.jQuery;
+  }
+
+  if (globalScope.$) {
+    return globalScope.$;
+  }
+
+  throw new Error(
+    "MagicBuyer: jQuery is not available in the current context."
+  );
+})();
 
 globalScope.$ = jquery;
 globalScope.jQuery = jquery;
+
 export default jquery;
